08115145fbd66a320b86a3ebd7d33574085fa707
Sol Jin <soljin01@gmail.com>
2017-04-25 00:21:24 -0400

    Change variable name from squares to currentBoard

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,12 +7,13 @@ class Game extends Component {
   constructor() {
     super();
     this.state = {
-      squares: Array(9).fill(null),
+      currentBoard: Array(9).fill(null),
       tIsNext: true,
       gameMessage: "Player T's Turn",
       buttonValue: "reset",
       gameFinished: false,
-      turnCount: 0
+      turnCount: 0,
+      score: [0,0]
     };
   }
 
@@ -27,26 +28,26 @@ square on the board to make a move
       return;
     }
     //get the current state of the board
-    let squares = this.state.squares
+    let currentBoard = this.state.currentBoard
 
     // Check to see if it is a valid move
-    if(!checkIfValidMove(i, squares)){
+    if(!checkIfValidMove(i, currentBoard)){
       this.setState({
         gameMessage: `Please make a valid move, Player ${this.state.tIsNext ? 'T' : 'P'}`
       });
       return;
     }
     //determine which token the current turn should be marked with
-    squares[i] = this.state.tIsNext ? 'T' : 'P';
+    currentBoard[i] = this.state.tIsNext ? 'T' : 'P';
     //check to see if there is a winner, returns "T", "P", or null
-    let winner = checkWinner(squares);
+    let winner = checkWinner(currentBoard);
     //true or false check to see if the board is filled or if there is a winner
     let gameFinished = (this.state.turnCount === 8 || winner) ? true : false
 
     //update the state with the new states
     this.setState({
       //current state of the board
-      squares: squares,
+      currentBoard: currentBoard,
       //determine which player is next
       tIsNext: !this.state.tIsNext,
       //determine message to display next player, or if game is won or tie
@@ -63,10 +64,9 @@ square on the board to make a move
   //event handler on the "reset"/"new game" button that starts a new game
   clearGame(){
     this.setState({
-      squares: Array(9).fill(null),
+      currentBoard: Array(9).fill(null),
       tIsNext: true,
       gameMessage: "Player T's Turn",
-      score: [0, 0],
       buttonValue: "reset",
       gameFinished: false,
       turnCount: 0
@@ -88,7 +88,7 @@ square on the board to make a move
       </div>
       <div className="game">
           <Board
-            squares={this.state.squares}
+            currentBoard={this.state.currentBoard}
             onClick={(i) => this.handleTurn(i)}
           />
       </div>
---
+++ src/components/Start.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Start extends Component {
+
+  render() {
+    return (
+      <button>Begin Game</button>
+    );
+  }
+}
+export default Start;
